a72ef5d91d24aa516126a79f099991ba81d106d4
Renzo Neps <39088521+Renzo-Neps@users.noreply.github.com>
2020-04-01 10:40:12 -0400

    Updates to the list page
    New style to the List page

--- Car Dealership Website Project/Car Lots R Us/Models/Car.cs
+++ Car Dealership Website Project/Car Lots R Us/Models/Car.cs
@@ -14,6 +14,6 @@
         public string Condition { get; set; }
         public string InventoryStatus { get; set; }
         public bool IsNew { get; set; }
-
+        public string ImageURL { get; set; }
     }
 }
