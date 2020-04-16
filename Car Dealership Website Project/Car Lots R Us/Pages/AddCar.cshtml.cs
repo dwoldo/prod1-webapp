2dde5c3fd53af530f3e18278ead0bc9a90df067a
Renzo Neps <39088521+Renzo-Neps@users.noreply.github.com>
2020-04-16 09:36:55 -0400

    New webpage added, Add Cars

---
+++ Car Dealership Website Project/Car Lots R Us/Pages/AddCar.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Car_Lots_R_Us.Pages
+{
+    public class AddCarModel : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
